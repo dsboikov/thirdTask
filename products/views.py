03d8b17a5d33203f5328740e38383300fcf76df6
Denis <dsboikov@yandex.ru>
2025-09-10 08:47:46 +0300

    DRF

--- products/views.py
+++ products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.filter(is_active=True)
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        category = self.request.query_params.get('category')
+        min_price = self.request.query_params.get('min_price')
+        max_price = self.request.query_params.get('max_price')
+        search = self.request.query_params.get('search')
+
+        if category:
+            qs = qs.filter(category__slug=category)
+        if min_price:
+            qs = qs.filter(price__gte=min_price)
+        if max_price:
+            qs = qs.filter(price__lte=max_price)
+        if search:
+            qs = qs.filter(name__icontains=search) | qs.filter(description__icontains=search)
+        return qs
